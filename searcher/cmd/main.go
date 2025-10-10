2b8b1960871647d4c135d72b69e34ae8c953d4f2
Beatriz Monteiro <biafigmonteiro@gmail.com>
2025-10-10 13:52:37 -0300

    add: channels and average price

--- searcher/cmd/main.go
+++ searcher/cmd/main.go
@@ -9,33 +9,42 @@ import (
 
 func main() {
 	start := time.Now()
-	var price1, price2, price3 float64
+	priceChannel := make(chan float64)
 	var wg sync.WaitGroup
 	wg.Add(3)
 
 	go func() {
 		defer wg.Done()
-		price1 = fetcher.FetchPriceFromSite1()
+		priceChannel <- fetcher.FetchPriceFromSite1()
 
 	}() //função anonima
 
 	go func() {
 		defer wg.Done()
-		price2 = fetcher.FetchPriceFromSite2()
+		priceChannel <- fetcher.FetchPriceFromSite2()
 
 	}()
 
 	go func() {
 		defer wg.Done()
-		price3 = fetcher.FetchPriceFromSite3()
+		priceChannel <- fetcher.FetchPriceFromSite3()
 
 	}()
 
-	wg.Wait()
+	go func() {
+		wg.Wait()
+		close(priceChannel)
+	}()
 
-	fmt.Printf("Preço do site 1: R$ %.2f\n", price1)
-	fmt.Printf("Preço do site 2: R$ %.2f\n", price2)
-	fmt.Printf("Preço do site 3: R$ %.2f\n", price3)
+	var totalPrice float64
+	var countPrices int
+
+	for price := range priceChannel {
+		totalPrice += price
+		countPrices++
+		avgPrice := totalPrice / float64(countPrices)
+		fmt.Printf("Preço recebido: R$ %.2f | Preço médio até agora: R$ %.2f\n", price, avgPrice)
+	}
 
 	fmt.Printf("\nTempo total: %s", time.Since(start))
 }
